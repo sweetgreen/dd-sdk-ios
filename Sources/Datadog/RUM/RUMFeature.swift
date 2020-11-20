fff658b886ac2728f943f976444c3d46b1a2b97f
Davor Bauk <davor.bauk@sweetgreen.com>
2020-11-20 12:55:14 -0800

    Add percent encoding to service tag.

--- Sources/Datadog/RUM/RUMFeature.swift
+++ Sources/Datadog/RUM/RUMFeature.swift
@@ -79,7 +79,7 @@ internal final class RUMFeature {
                     .batchTime(using: commonDependencies.dateProvider),
                     .ddtags(
                         tags: [
-                            "service:\(configuration.common.serviceName)",
+                            "service:\(configuration.common.serviceName.addingPercentEncoding(withAllowedCharacters: .ddURLQueryAllowed)))",
                             "version:\(configuration.common.applicationVersion)",
                             "sdk_version:\(sdkVersion)",
                             "env:\(configuration.common.environment)"
@@ -125,4 +125,24 @@ internal final class RUMFeature {
         self.storage = storage
         self.upload = upload
     }
+}
+
+extension CharacterSet {
+    /// Creates a CharacterSet from RFC 3986 allowed characters.
+    ///
+    /// RFC 3986 states that the following characters are "reserved" characters.
+    ///
+    /// - General Delimiters: ":", "#", "[", "]", "@", "?", "/"
+    /// - Sub-Delimiters: "!", "$", "&", "'", "(", ")", "*", "+", ",", ";", "="
+    ///
+    /// In RFC 3986 - Section 3.4, it states that the "?" and "/" characters should not be escaped to allow
+    /// query strings to include a URL. Therefore, all "reserved" characters with the exception of "?" and "/"
+    /// should be percent-escaped in the query string.
+    public static let ddURLQueryAllowed: CharacterSet = {
+        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
+        let subDelimitersToEncode = "!$&'()*+,;="
+        let encodableDelimiters = CharacterSet(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
+
+        return CharacterSet.urlQueryAllowed.subtracting(encodableDelimiters)
+    }()
 }
